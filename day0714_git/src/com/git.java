cb2433e243a3e2a5fdac065e984d5c711be81f68
qs <qi123582011@163.com>
2020-07-14 14:35:57 +0800

    第一次修改

---
+++ day0714_git/src/com/git.java
@@ -0,0 +1,10 @@
+package com;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+
+    }
+}
